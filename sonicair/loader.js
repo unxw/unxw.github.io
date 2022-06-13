49c909e053a55d7a38bbad7bdb709464f731346c
unxw <99024112+unxw@users.noreply.github.com>
2022-06-13 14:08:01 -0400

    Add files via upload

---
+++ sonicair/loader.js
@@ -0,0 +1,350 @@
+
+  var Module = typeof Module !== 'undefined' ? Module : {};
+  
+  if (!Module.expectedDataFileDownloads) {
+    Module.expectedDataFileDownloads = 0;
+  }
+  Module.expectedDataFileDownloads++;
+  (function() {
+   var loadPackage = function(metadata) {
+  
+      var PACKAGE_PATH;
+      if (typeof window === 'object') {
+        PACKAGE_PATH = window['encodeURIComponent'](window.location.pathname.toString().substring(0, window.location.pathname.toString().lastIndexOf('/')) + '/');
+      } else if (typeof location !== 'undefined') {
+        // worker
+        PACKAGE_PATH = encodeURIComponent(location.pathname.toString().substring(0, location.pathname.toString().lastIndexOf('/')) + '/');
+      } else {
+        throw 'using preloaded data can only be done on a web page or in a web worker';
+      }
+      var PACKAGE_NAME = 'source/_emscripten/dist/sonic3air.data';
+      var REMOTE_PACKAGE_BASE = 'https://files.catbox.moe/28acwm.data';
+      if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
+        Module['locateFile'] = Module['locateFilePackage'];
+        err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
+      }
+      var REMOTE_PACKAGE_NAME = Module['locateFile'] ? Module['locateFile'](REMOTE_PACKAGE_BASE, '') : REMOTE_PACKAGE_BASE;
+    
+      var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
+      var PACKAGE_UUID = metadata['package_uuid'];
+    
+      function fetchRemotePackage(packageName, packageSize, callback, errback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', packageName, true);
+        xhr.responseType = 'arraybuffer';
+        xhr.onprogress = function(event) {
+          var url = packageName;
+          var size = packageSize;
+          if (event.total) size = event.total;
+          if (event.loaded) {
+            if (!xhr.addedTotal) {
+              xhr.addedTotal = true;
+              if (!Module.dataFileDownloads) Module.dataFileDownloads = {};
+              Module.dataFileDownloads[url] = {
+                loaded: event.loaded,
+                total: size
+              };
+            } else {
+              Module.dataFileDownloads[url].loaded = event.loaded;
+            }
+            var total = 0;
+            var loaded = 0;
+            var num = 0;
+            for (var download in Module.dataFileDownloads) {
+            var data = Module.dataFileDownloads[download];
+              total += data.total;
+              loaded += data.loaded;
+              num++;
+            }
+            total = Math.ceil(total * Module.expectedDataFileDownloads/num);
+            if (Module['setStatus']) Module['setStatus']('Downloading data... (' + loaded + '/' + total + ')');
+          } else if (!Module.dataFileDownloads) {
+            if (Module['setStatus']) Module['setStatus']('Downloading data...');
+          }
+        };
+        xhr.onerror = function(event) {
+          throw new Error("NetworkError for: " + packageName);
+        }
+        xhr.onload = function(event) {
+          if (xhr.status == 200 || xhr.status == 304 || xhr.status == 206 || (xhr.status == 0 && xhr.response)) { // file URLs can return 0
+            var packageData = xhr.response;
+            callback(packageData);
+          } else {
+            throw new Error(xhr.statusText + " : " + xhr.responseURL);
+          }
+        };
+        xhr.send(null);
+      };
+
+      function handleError(error) {
+        console.error('package error:', error);
+      };
+    
+    function runWithFS() {
+  
+      function assert(check, msg) {
+        if (!check) throw msg + new Error().stack;
+      }
+  Module['FS_createPath']('/', 'sonic3air', true, true);
+Module['FS_createPath']('/sonic3air', 'data', true, true);
+Module['FS_createPath']('/sonic3air/data', 'font', true, true);
+
+      /** @constructor */
+      function DataRequest(start, end, audio) {
+        this.start = start;
+        this.end = end;
+        this.audio = audio;
+      }
+      DataRequest.prototype = {
+        requests: {},
+        open: function(mode, name) {
+          this.name = name;
+          this.requests[name] = this;
+          Module['addRunDependency']('fp ' + this.name);
+        },
+        send: function() {},
+        onload: function() {
+          var byteArray = this.byteArray.subarray(this.start, this.end);
+          this.finish(byteArray);
+        },
+        finish: function(byteArray) {
+          var that = this;
+  
+          Module['FS_createDataFile'](this.name, null, byteArray, true, true, true); // canOwn this data in the filesystem, it is a slide into the heap that will never change
+          Module['removeRunDependency']('fp ' + that.name);
+  
+          this.requests[this.name] = null;
+        }
+      };
+  
+          var files = metadata['files'];
+          for (var i = 0; i < files.length; ++i) {
+            new DataRequest(files[i]['start'], files[i]['end'], files[i]['audio']).open('GET', files[i]['filename']);
+          }
+  
+    
+        var indexedDB;
+        if (typeof window === 'object') {
+          indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+        } else if (typeof location !== 'undefined') {
+          // worker
+          indexedDB = self.indexedDB;
+        } else {
+          throw 'using IndexedDB to cache data can only be done on a web page or in a web worker';
+        }
+        var IDB_RO = "readonly";
+        var IDB_RW = "readwrite";
+        var DB_NAME = "EM_PRELOAD_CACHE";
+        var DB_VERSION = 1;
+        var METADATA_STORE_NAME = 'METADATA';
+        var PACKAGE_STORE_NAME = 'PACKAGES';
+        function openDatabase(callback, errback) {
+          try {
+            var openRequest = indexedDB.open(DB_NAME, DB_VERSION);
+          } catch (e) {
+            return errback(e);
+          }
+          openRequest.onupgradeneeded = function(event) {
+            var db = event.target.result;
+
+            if(db.objectStoreNames.contains(PACKAGE_STORE_NAME)) {
+              db.deleteObjectStore(PACKAGE_STORE_NAME);
+            }
+            var packages = db.createObjectStore(PACKAGE_STORE_NAME);
+
+            if(db.objectStoreNames.contains(METADATA_STORE_NAME)) {
+              db.deleteObjectStore(METADATA_STORE_NAME);
+            }
+            var metadata = db.createObjectStore(METADATA_STORE_NAME);
+          };
+          openRequest.onsuccess = function(event) {
+            var db = event.target.result;
+            callback(db);
+          };
+          openRequest.onerror = function(error) {
+            errback(error);
+          };
+        };
+
+        // This is needed as chromium has a limit on per-entry files in IndexedDB
+        // https://cs.chromium.org/chromium/src/content/renderer/indexed_db/webidbdatabase_impl.cc?type=cs&sq=package:chromium&g=0&l=177
+        // https://cs.chromium.org/chromium/src/out/Debug/gen/third_party/blink/public/mojom/indexeddb/indexeddb.mojom.h?type=cs&sq=package:chromium&g=0&l=60
+        // We set the chunk size to 64MB to stay well-below the limit
+        var CHUNK_SIZE = 64 * 1024 * 1024;
+
+        function cacheRemotePackage(
+          db,
+          packageName,
+          packageData,
+          packageMeta,
+          callback,
+          errback
+        ) {
+          var transactionPackages = db.transaction([PACKAGE_STORE_NAME], IDB_RW);
+          var packages = transactionPackages.objectStore(PACKAGE_STORE_NAME);
+          var chunkSliceStart = 0;
+          var nextChunkSliceStart = 0;
+          var chunkCount = Math.ceil(packageData.byteLength / CHUNK_SIZE);
+          var finishedChunks = 0;
+          for (var chunkId = 0; chunkId < chunkCount; chunkId++) {
+            nextChunkSliceStart += CHUNK_SIZE;
+            var putPackageRequest = packages.put(
+              packageData.slice(chunkSliceStart, nextChunkSliceStart),
+              'package/' + packageName + '/' + chunkId
+            );
+            chunkSliceStart = nextChunkSliceStart;
+            putPackageRequest.onsuccess = function(event) {
+              finishedChunks++;
+              if (finishedChunks == chunkCount) {
+                var transaction_metadata = db.transaction(
+                  [METADATA_STORE_NAME],
+                  IDB_RW
+                );
+                var metadata = transaction_metadata.objectStore(METADATA_STORE_NAME);
+                var putMetadataRequest = metadata.put(
+                  {
+                    'uuid': packageMeta.uuid,
+                    'chunkCount': chunkCount
+                  },
+                  'metadata/' + packageName
+                );
+                putMetadataRequest.onsuccess = function(event) {
+                  callback(packageData);
+                };
+                putMetadataRequest.onerror = function(error) {
+                  errback(error);
+                };
+              }
+            };
+            putPackageRequest.onerror = function(error) {
+              errback(error);
+            };
+          }
+        }
+
+        /* Check if there's a cached package, and if so whether it's the latest available */
+        function checkCachedPackage(db, packageName, callback, errback) {
+          var transaction = db.transaction([METADATA_STORE_NAME], IDB_RO);
+          var metadata = transaction.objectStore(METADATA_STORE_NAME);
+          var getRequest = metadata.get('metadata/' + packageName);
+          getRequest.onsuccess = function(event) {
+            var result = event.target.result;
+            if (!result) {
+              return callback(false, null);
+            } else {
+              return callback(PACKAGE_UUID === result['uuid'], result);
+            }
+          };
+          getRequest.onerror = function(error) {
+            errback(error);
+          };
+        }
+
+        function fetchCachedPackage(db, packageName, metadata, callback, errback) {
+          var transaction = db.transaction([PACKAGE_STORE_NAME], IDB_RO);
+          var packages = transaction.objectStore(PACKAGE_STORE_NAME);
+
+          var chunksDone = 0;
+          var totalSize = 0;
+          var chunkCount = metadata['chunkCount'];
+          var chunks = new Array(chunkCount);
+
+          for (var chunkId = 0; chunkId < chunkCount; chunkId++) {
+            var getRequest = packages.get('package/' + packageName + '/' + chunkId);
+            getRequest.onsuccess = function(event) {
+              // If there's only 1 chunk, there's nothing to concatenate it with so we can just return it now
+              if (chunkCount == 1) {
+                callback(event.target.result);
+              } else {
+                chunksDone++;
+                totalSize += event.target.result.byteLength;
+                chunks.push(event.target.result);
+                if (chunksDone == chunkCount) {
+                  if (chunksDone == 1) {
+                    callback(event.target.result);
+                  } else {
+                    var tempTyped = new Uint8Array(totalSize);
+                    var byteOffset = 0;
+                    for (var chunkId in chunks) {
+                      var buffer = chunks[chunkId];
+                      tempTyped.set(new Uint8Array(buffer), byteOffset);
+                      byteOffset += buffer.byteLength;
+                      buffer = undefined;
+                    }
+                    chunks = undefined;
+                    callback(tempTyped.buffer);
+                    tempTyped = undefined;
+                  }
+                }
+              }
+            };
+            getRequest.onerror = function(error) {
+              errback(error);
+            };
+          }
+        }
+      
+      function processPackageData(arrayBuffer) {
+        assert(arrayBuffer, 'Loading data file failed.');
+        assert(arrayBuffer instanceof ArrayBuffer, 'bad input to processPackageData');
+        var byteArray = new Uint8Array(arrayBuffer);
+        var curr;
+        
+          // Reuse the bytearray from the XHR as the source for file reads.
+          DataRequest.prototype.byteArray = byteArray;
+    
+            var files = metadata['files'];
+            for (var i = 0; i < files.length; ++i) {
+              DataRequest.prototype.requests[files[i].filename].onload();
+            }
+                Module['removeRunDependency']('datafile_source/_emscripten/dist/sonic3air.data');
+
+      };
+      Module['addRunDependency']('datafile_source/_emscripten/dist/sonic3air.data');
+    
+      if (!Module.preloadResults) Module.preloadResults = {};
+    
+        function preloadFallback(error) {
+          console.error(error);
+          console.error('falling back to default preload behavior');
+          fetchRemotePackage(REMOTE_PACKAGE_NAME, REMOTE_PACKAGE_SIZE, processPackageData, handleError);
+        };
+
+        openDatabase(
+          function(db) {
+            checkCachedPackage(db, PACKAGE_PATH + PACKAGE_NAME,
+              function(useCached, metadata) {
+                Module.preloadResults[PACKAGE_NAME] = {fromCache: useCached};
+                if (useCached) {
+                  fetchCachedPackage(db, PACKAGE_PATH + PACKAGE_NAME, metadata, processPackageData, preloadFallback);
+                } else {
+                  fetchRemotePackage(REMOTE_PACKAGE_NAME, REMOTE_PACKAGE_SIZE,
+                    function(packageData) {
+                      cacheRemotePackage(db, PACKAGE_PATH + PACKAGE_NAME, packageData, {uuid:PACKAGE_UUID}, processPackageData,
+                        function(error) {
+                          console.error(error);
+                          processPackageData(packageData);
+                        });
+                    }
+                  , preloadFallback);
+                }
+              }
+            , preloadFallback);
+          }
+        , preloadFallback);
+
+        if (Module['setStatus']) Module['setStatus']('Downloading...');
+      
+    }
+    if (Module['calledRun']) {
+      runWithFS();
+    } else {
+      if (!Module['preRun']) Module['preRun'] = [];
+      Module["preRun"].push(runWithFS); // FS is not initialized yet, wait for it
+    }
+  
+   }
+   loadPackage({"files": [{"filename": "/sonic3air/config.json", "start": 0, "end": 2864, "audio": 0}, {"filename": "/sonic3air/data/audiodata.bin", "start": 2864, "end": 2204857, "audio": 0}, {"filename": "/sonic3air/data/audioremaster.bin", "start": 2204857, "end": 133883696, "audio": 0}, {"filename": "/sonic3air/data/enginedata.bin", "start": 133883696, "end": 133957519, "audio": 0}, {"filename": "/sonic3air/data/gamedata.bin", "start": 133957519, "end": 138097102, "audio": 0}, {"filename": "/sonic3air/data/metadata.json", "start": 138097102, "end": 138097264, "audio": 0}, {"filename": "/sonic3air/data/font/freefont_pixeled.json", "start": 138097264, "end": 138099511, "audio": 0}, {"filename": "/sonic3air/data/font/freefont_pixeled.png", "start": 138099511, "end": 138101232, "audio": 0}, {"filename": "/sonic3air/data/font/freefont_pixeled_light.json", "start": 138101232, "end": 138103038, "audio": 0}, {"filename": "/sonic3air/data/font/freefont_pixeled_light.png", "start": 138103038, "end": 138104079, "audio": 0}, {"filename": "/sonic3air/data/font/freefont_pixeled_small.json", "start": 138104079, "end": 138106383, "audio": 0}, {"filename": "/sonic3air/data/font/freefont_pixeled_small.png", "start": 138106383, "end": 138107591, "audio": 0}, {"filename": "/sonic3air/data/font/freefont_pixeled_tiny.json", "start": 138107591, "end": 138109718, "audio": 0}, {"filename": "/sonic3air/data/font/freefont_pixeled_tiny.png", "start": 138109718, "end": 138110668, "audio": 0}, {"filename": "/sonic3air/data/font/freefont_pixeled_tiny_narrow.json", "start": 138110668, "end": 138112798, "audio": 0}, {"filename": "/sonic3air/data/font/freefont_pixeled_tiny_narrow.png", "start": 138112798, "end": 138113716, "audio": 0}, {"filename": "/sonic3air/data/font/freefont_sampled.json", "start": 138113716, "end": 138115937, "audio": 0}, {"filename": "/sonic3air/data/font/freefont_sampled.png", "start": 138115937, "end": 138122567, "audio": 0}, {"filename": "/sonic3air/data/font/monofont.json", "start": 138122567, "end": 138124805, "audio": 0}, {"filename": "/sonic3air/data/font/monofont.png", "start": 138124805, "end": 138126153, "audio": 0}, {"filename": "/sonic3air/data/font/smallfont.json", "start": 138126153, "end": 138127103, "audio": 0}, {"filename": "/sonic3air/data/font/smallfont.png", "start": 138127103, "end": 138127549, "audio": 0}, {"filename": "/sonic3air/data/font/sonic3_fontB.json", "start": 138127549, "end": 138129223, "audio": 0}, {"filename": "/sonic3air/data/font/sonic3_fontB.png", "start": 138129223, "end": 138130192, "audio": 0}, {"filename": "/sonic3air/data/font/sonic3_fontC.json", "start": 138130192, "end": 138131888, "audio": 0}, {"filename": "/sonic3air/data/font/sonic3_fontC.png", "start": 138131888, "end": 138133115, "audio": 0}, {"filename": "/sonic3air/data/scripts.bin", "start": 138133115, "end": 144259384, "audio": 0}], "remote_package_size": 144259384, "package_uuid": "e7ebc096-dec4-45f0-881e-461c2586af21"});
+  
+  })();
+  
