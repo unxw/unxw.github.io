894169e0766585980a340a543261fc9f4ef3f50b
i hate coding <99024112+unxw@users.noreply.github.com>
2023-04-03 08:47:53 -0400

    update 9.1

---
+++ js/dumbwelcomealert.js
@@ -0,0 +1,1 @@
+alert("| Update 9.1x || no updates for a while, have essay to do || added bitlife! still working on fixing shellshockers and stuff  |")
---
+++ js/outputlog.js
@@ -0,0 +1,1 @@
+console.log('%cwhy are you going through my code... at least credit me if anything :)   -unxw', 'font-size:30px; color: green; font-family: "Open Sans"')
