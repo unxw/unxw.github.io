262a177f89cf805e71c202ab01889342b7bde165
unxw sucks at coding <99024112+unxw@users.noreply.github.com>
2023-05-08 15:02:28 -0400

    Update dumbwelcomealert.js

--- js/dumbwelcomealert.js
+++ js/dumbwelcomealert.js
@@ -1,1 +1,1 @@
-alert("| Update 9.4.x ||no updates for a long time because i have a stupid english isu to do. btw if yk this site, then we are likey from the same school, so do ur work 💀💀💀|)
+alert("| Update 9.4.x ||no updates for a long time because i have a stupid english isu to do. btw if yk this site, then we are likey from the same school, so do ur work 💀💀💀|")
